631232c59e7973fab91ad750d493086c7aab9f91
shaoxinyu <2424590457@qq.com>
2022-07-22 00:06:12 +0800

    新增几行测试,bytebuffer

--- src/test/java/com/threekilled/ThreekilledApplicationTests.java
+++ src/test/java/com/threekilled/ThreekilledApplicationTests.java
@@ -3,11 +3,38 @@ package com.threekilled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ThreekilledApplicationTests {
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
-    @Test
-    void contextLoads() {
+@SpringBootTest
+class ThreeKilledApplicationTests {
+
+    public static void main(String[] args) {
+        //FileChannel
+        //获取方式 1.输入输出流 2.RandomAccessFile
+        try(FileChannel fileChannel = new FileInputStream("data.txt").getChannel()) {
+            //准备缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            while (true){
+                //从channel读取数据,向buffer写入
+                int read = fileChannel.read(buffer);
+                if (read==-1){
+                    break;
+                }
+                //打印buffer
+                //切换buffer的读模式
+                buffer.flip();
+                while (buffer.hasRemaining()){
+                    byte b = buffer.get();
+                    System.out.println((char) b);
+                }
+                //切换成写模式
+                buffer.clear();
+            }
+        }catch(Exception e){
+
+        }
     }
 
 }
