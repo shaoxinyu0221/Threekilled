6490c9f3af6b077c01cee3db73bca29e13864781
邵鑫雨 <2424590457@qq.com>
2022-07-21 22:20:29 +0800

    添加依赖和启动项改名

--- src/main/java/com/threekilled/ThreekilledApplication.java
+++ src/main/java/com/threekilled/ThreekilledApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThreekilledApplication {
+public class ThreeKilledApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ThreekilledApplication.class, args);
+        SpringApplication.run(ThreeKilledApplication.class, args);
     }
 
 }
